00d25e7f26af2b50a9f0f782e3aa200f9258e090
Nilana Hansaja <138865043+Hansajaa@users.noreply.github.com>
2023-12-11 10:09:37 +0530

    initial commit

---
+++ jasperReport/src/main/java/Controller/DashboardController.java
@@ -0,0 +1,40 @@
+package Controller;
+
+import DB.DBConnection;
+import javafx.event.ActionEvent;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
+
+import java.sql.SQLException;
+
+public class DashboardController {
+
+    public void customerReportOnAction(ActionEvent actionEvent) throws JRException {
+        try {
+            JasperDesign design = JRXmlLoader.load("src/main/resources/report/Customer-Report.jrxml");
+            //
+            //
+            JasperReport jasperReport = JasperCompileManager.compileReport(design);
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null, DBConnection.getInstance().getConnection());
+            JasperViewer.viewReport(jasperPrint,false);
+        } catch (JRException | ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void itemReportOnAction(ActionEvent actionEvent) {
+        try {
+            JasperDesign design = JRXmlLoader.load("src/main/resources/report/item_report.jrxml");
+            //
+            //
+            JasperReport jasperReport = JasperCompileManager.compileReport(design);
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null, DBConnection.getInstance().getConnection());
+            JasperViewer.viewReport(jasperPrint,false);
+        } catch (JRException | ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
